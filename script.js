4e9d55c4fcbd4b393e91e8e3abc5dea6ada7ef3e
GA3RlEL <kruszkagabrys@gmail.com>
2022-06-08 14:41:35 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,56 @@
+var generatebutton = document.getElementById("genpass");
+
+generatebutton.addEventListener("click", () => {
+    let checkedTab = [];
+    let passlen = document.getElementById("len").value;
+    var password = "";
+    let randomL;
+
+    let checkbox = document.getElementsByClassName("chkbx")
+
+
+    //check what checkboxes are checked
+    for (let i = 0; i < checkbox.length; i++) {
+        if (checkbox[i].checked == true) {
+            checkedTab.push((i + 1));
+        } else if (checkbox[i].checked == false)
+            checkedTab.push(0);
+    }
+
+    //delete 0 from array
+    for (let i = 0; i < checkedTab.length; i++) {
+        if (checkedTab[i] === 0) {
+            checkedTab.splice(i, 1);
+            i--;
+        }
+
+    }
+
+    console.log(checkedTab);
+
+
+    for (let i = 0; i < passlen; i++) {
+        let random = Math.floor(Math.random() * (checkedTab.length))
+
+        let option = checkedTab[random]
+        if (option == 1) {
+            randomL = Math.floor(Math.random() * 25 + 97);
+            password = password + String.fromCharCode(randomL);
+        } else if (option == 2) {
+            randomL = Math.floor(Math.random() * 25 + 65);
+            password = password + String.fromCharCode(randomL);
+        } else if (option == 3) {
+            randomL = Math.floor(Math.random() * 10 + 48);
+            password = password + String.fromCharCode(randomL);
+        } else if (option == 4){
+            randomL = Math.floor(Math.random() * 6 +33);
+            password = password + String.fromCharCode(randomL);
+        }
+    }
+
+    let output=document.getElementById("output");
+    output.setAttribute("value",password);
+
+
+
+})
