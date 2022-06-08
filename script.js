6e7283e3544fdccd6a504b166906effe5ed8dbcb
GA3RlEL <kruszkagabrys@gmail.com>
2022-06-08 16:57:37 +0200

    animation click

--- script.js
+++ script.js
@@ -26,7 +26,6 @@ generatebutton.addEventListener("click", () => {
 
     }
 
-    console.log(checkedTab);
 
 
     for (let i = 0; i < passlen; i++) {
@@ -42,15 +41,38 @@ generatebutton.addEventListener("click", () => {
         } else if (option == 3) {
             randomL = Math.floor(Math.random() * 10 + 48);
             password = password + String.fromCharCode(randomL);
-        } else if (option == 4){
-            randomL = Math.floor(Math.random() * 6 +33);
+        } else if (option == 4) {
+            randomL = Math.floor(Math.random() * 6 + 33);
             password = password + String.fromCharCode(randomL);
         }
     }
 
-    let output=document.getElementById("output");
-    output.setAttribute("value",password);
+    let output = document.getElementById("output");
+    output.setAttribute("value", password);
 
 
 
+    generatebutton.classList.add("active")
+    setInterval(() => {
+        generatebutton.classList.remove("active")
+    }, 300)
+})
+
+
+//copy to clipboard
+
+var buttonToCopy = document.getElementById("copy");
+
+buttonToCopy.addEventListener("click", () => {
+    var copyText = document.getElementById("output");
+
+    copyText.select();
+    copyText.setSelectionRange(0, 999999);
+
+    navigator.clipboard.writeText(copyText.value);
+
+    buttonToCopy.classList.add("active")
+    setInterval(() => {
+        buttonToCopy.classList.remove("active")
+    }, 300)
 })
